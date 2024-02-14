03ba4d2de8e0010101043e367121a7f49659db92
Anmoldeep Singh <124416018+AnmoldeepSingh-Sandhu@users.noreply.github.com>
2024-02-13 22:37:43 -0800

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDom from 'react-dom';
-import React from 'react';
-
-import App from './App.js';
-import "./style.css";
-
-ReactDom.render(<App />, document.getElementById("project-container"));
